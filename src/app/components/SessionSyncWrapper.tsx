82f8899924029295bc6dc1eb0dfda22c031b0b26
Esseh12 <ese.gbenmo@salario.africa>
2025-11-03 02:29:30 +0100

    removed session storage

--- src/app/components/SessionSyncWrapper.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import useSyncSessionToLocalStorage from '@/hooks/useSyncSessionToLocalStorage';
-
-export default function SessionSyncWrapper() {
-	useSyncSessionToLocalStorage();
-	return null; // it doesn't render anything
-}
